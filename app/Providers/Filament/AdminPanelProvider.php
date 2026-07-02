aa3873d1517adc150ff1d8d70bac3390603cace1
haqimiM9 <it.merchant9@outlook.com>
2026-07-02 17:58:34 +0800

    Enhance admin panel configuration by adding brand name and enabling collapsible sidebar on desktop

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,6 +40,8 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->brandName('Merchant9 OS')
+            ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
