dbcf922775acd9fa9f16c689c22928d7d57a6beb
haqimiM9 <it.merchant9@outlook.com>
2026-07-23 12:50:27 +0800

    updated: stats daily asset positions

--- app/Filament/Widgets/DailyAssetPositionListSummary.php
+++ app/Filament/Widgets/DailyAssetPositionListSummary.php
@@ -34,6 +34,7 @@ class DailyAssetPositionListSummary extends StatsOverviewWidget
             Stat::make('Closing Stock', number_format($summary['closing_stock_weight'], 3).' g'),
             Stat::make('Net Weight', number_format($summary['net_weight'], 3).' g'),
             Stat::make('Available Cash', 'RM '.number_format($summary['available_cash'], 2)),
+            Stat::make('Cash For GB', 'RM '.number_format($summary['cash_for_gb'], 2)),
             Stat::make('Supplier Hutang', number_format($summary['supplier_hutang'], 3).' g')
                 ->color($summary['supplier_hutang'] > 0 ? 'warning' : 'gray'),
             Stat::make('Supplier Overpaid', number_format($summary['supplier_overpaid'], 3).' g')
